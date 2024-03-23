a20514de564fef04666c8afbdded33aeb59e4818
Eric Beauce <ebeauce@ldeo.columbia.edu>
2024-03-23 09:32:49 -0400

    Start in-depth cleaning

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 try:
     version = os.environ["CI_COMMIT_TAG"]
 except KeyError:
-    version = "0.x.x"
+    version = "2.0.0dev"
 
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
